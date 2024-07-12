43fbc020f9cae12a0bcfa51f97978770bf0be72c
Leo <136071261+LeoPerluTuru@users.noreply.github.com>
2024-07-12 11:48:08 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        'chillax' : ['Chillax', 'sams-serif'],
+        'inter' : ['Inter', 'sans-serif'],
+        'satoshi' : ['Satoshi', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
